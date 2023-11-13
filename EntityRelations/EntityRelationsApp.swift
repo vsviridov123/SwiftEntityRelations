10e9f5b68f7ef68c45c2cc1548aa798dbd02e4a6
Vladislav Sviridov <sviridov-vlad@yandex-team.ru>
2023-11-13 17:06:19 +0300

    Initial Commit

---
+++ EntityRelations/EntityRelationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntityRelationsApp.swift
+//  EntityRelations
+//
+//  Created by Vladislav Sviridov on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct EntityRelationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
